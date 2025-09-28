989bd7eae6d4865b2e98a713dd9bf71471eb4bec
Samuele Fiorini <sfiorini@c1r3p4.42firenze.it>
2025-09-28 16:00:59 +0200

    inception, in teoria finito

---
+++ inception_daPushare/srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+WP_PATH=/var/www/html
+
+echo "Waiting for MariaDB to be ready..."
+
+until mysqladmin ping -h "mariadb" --silent; do
+    echo "Waiting..."
+    sleep 1
+done
+echo "MariaDB is ready!"
+
+if [ ! -f "$WP_PATH/wp-config.php" ]; then
+    echo "Downloading WordPress core..."
+    wp core download --allow-root
+    echo "Creating wp-config.php..."
+    wp config create --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=mariadb --allow-root
+    echo "Installing WordPress..."
+    wp core install --url=${DOMAIN_NAME} --title="sfiorini's site" --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --allow-root
+else
+    echo "WordPress already installed..."
+fi
+
+
+if ! wp user get ${WORDPRESS_USER} --path=$WP_PATH --allow-root > /dev/null 2>&1; then
+    echo "Creating user ${WORDPRESS_USER}..."
+    wp user create ${WORDPRESS_USER} ${WORDPRESS_EMAIL} --role=author --user_pass=${WORDPRESS_PASSWORD} --path=$WP_PATH --allow-root
+else
+    echo "User ${WORDPRESS_USER} already exists."
+fi
+
+wp option update siteurl 'https://sfiorini.42.fr' --allow-root
+wp option update home 'https://sfiorini.42.fr' --allow-root
+
+
+echo "Starting PHP-FPM..."
+
+exec php-fpm7.4 -F
